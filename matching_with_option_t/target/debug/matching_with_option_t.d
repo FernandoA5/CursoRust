86532e9b58f8d0c2e1aa1126350afc90c02933ef
Fernando Alcalá <fernandoalcalacasas@hotmail.com>
2023-02-07 19:38:33 -0600

    Testeado código de episodio 43

---
+++ matching_with_option_t/target/debug/matching_with_option_t.d
@@ -0,0 +1,1 @@
+/home/alcss/Documentos/Programación/CursoRust/matching_with_option_t/target/debug/matching_with_option_t: /home/alcss/Documentos/Programación/CursoRust/matching_with_option_t/src/main.rs
